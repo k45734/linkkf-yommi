e13ff34f57d28cd42f3293308ede3297707b668f
k45734 <71308262+k45734@users.noreply.github.com>
2023-03-17 08:35:15 +0900

    Update logic_linkkfyommi.py

--- logic_linkkfyommi.py
+++ logic_linkkfyommi.py
@@ -650,7 +650,7 @@ class LogicLinkkfYommi(object):
                 #xpath_select_query = '//select[@class="switcher"]/option'
                 #xpath_select_query = "//select/option"
                 #xpath_select_query = '//*[@id="selectsv"]'
-                url_list = re.compile("player_post\('https:\/\/.*?'").findall(html)
+                url_list = re.compile("player_post\('https:\/\/.*?'").findall(iframe_url)
                 logger.debug(url_list)
                 for i in url_list:
                     logger.debug(i)
