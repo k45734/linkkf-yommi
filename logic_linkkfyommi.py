d6f3eb1776de20161ecd0e66a4451e207ba3e8f7
k45734 <71308262+k45734@users.noreply.github.com>
2023-04-10 21:07:08 +0900

    Update logic_linkkfyommi.py

--- logic_linkkfyommi.py
+++ logic_linkkfyommi.py
@@ -598,7 +598,7 @@ class LogicLinkkfYommi(object):
                     break
                 except:
                     pass
-            xpath_select_query = '//select[@class="switcher"]/option'
+            xpath_select_query = '//*[@id="body"]/div/span/center/select'
             #pattern = re.compile("'https:\/\/.*?kfani.me\/.*?'").findall(html_data)
           #  logger.debug(pattern)
            # logger.debug(f"dev:: {len(tree.xpath(xpath_select_query))}")
