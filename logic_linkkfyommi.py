5f0a4ab6c380f2647ccaa48a0b7fd58774f7148d
k45734 <71308262+k45734@users.noreply.github.com>
2023-07-22 15:53:41 +0900

    Update logic_linkkfyommi.py

--- logic_linkkfyommi.py
+++ logic_linkkfyommi.py
@@ -1330,17 +1330,17 @@ class LogicLinkkfYommi(object):
             if match:
                 epi_no_ckeck = match.group("epi_no")
                 logger.debug('test %s', epi_no_ckeck)
-                if ' ' in epi_no_ckeck:
-                    tes = epi_no_ckeck.find(' ')
-                    epi_no = epi_no_ckeck[0:tes]
+                if ' ' in title:
+                    tes = title.find(' ')
+                    epi_no = title[0:tes]
                     logger.debug('test1 %s', epi_no)
-                elif 'OVA' in epi_no_ckeck:
-                    tes = epi_no_ckeck.find('OVA')
-                    epi_no = epi_no_ckeck[0:tes]
+                elif 'OVA' in title:
+                    tes = title.find('OVA')
+                    epi_no = title[0:tes]
                     logger.debug('test2 %s', epi_no)
-                elif 'SP' in epi_no_ckeck:
-                    tes = epi_no_ckeck.find('SP')
-                    epi_no = epi_no_ckeck[0:tes]
+                elif 'SP' in title:
+                    tes = title.find('SP')
+                    epi_no = title[0:tes]
                     logger.debug('test3 %s', epi_no)
                 else:
                     epi_no = int(match.group("epi_no"))
