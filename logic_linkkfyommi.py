3c4f7740be16666c37de344a8606e9689c196b4f
k45734 <71308262+k45734@users.noreply.github.com>
2025-10-30 09:16:05 +0900

    Update logic_linkkfyommi.py

--- logic_linkkfyommi.py
+++ logic_linkkfyommi.py
@@ -379,6 +379,7 @@ class LogicLinkkfYommi(object):
                 #data = LogicLinkkfYommi.get_html(url2)
                 referer_url = 'https://linkkf.tv/'
                 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36','referer': referer_url}
+                data = session.get(url,headers=headers)
                 data = session.get(url2,headers=headers)
                 #logger.debug(data.status_code)
                 logger.debug("source code ::: %s", data)
